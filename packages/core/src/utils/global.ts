7ba8ea9717e6f55567ceef62a89f31dd52ced7b4
heaijun <1556087626@qq.com>
2024-05-07 11:09:59 +0800

    feat: 初始化仓库

---
+++ packages/core/src/utils/global.ts
@@ -0,0 +1,41 @@
+import { isWindow } from './is'
+import { WebTracing } from '../types'
+
+/**
+ * 是否为浏览器环境
+ */
+export const isBrowserEnv = isWindow(typeof window !== 'undefined' ? window : 0)
+
+/**
+ * 是否为 electron 环境
+ */
+export const isElectronEnv = !!window?.process?.versions?.electron
+
+/**
+ * 获取全局变量
+ */
+export function getGlobal(): Window {
+  if (isBrowserEnv || isElectronEnv) return window
+  return {} as Window
+}
+
+/**
+ * 获取全部变量 __webTracing__ 的引用地址
+ */
+export function getGlobalSupport(): WebTracing {
+  _global.__webTracing__ = _global.__webTracing__ || ({} as WebTracing)
+  return _global.__webTracing__
+}
+
+/**
+ * 判断sdk是否初始化
+ * @returns sdk是否初始化
+ */
+export function isInit(): boolean {
+  return !!_global.__webTracingInit__
+}
+
+const _global = getGlobal()
+const _support = getGlobalSupport()
+
+export { _global, _support }
